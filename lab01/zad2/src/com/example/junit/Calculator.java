2c7db512b96c42b3e0771b03d65ebc1ee655d0ab
Dariusz Buszman <dbuszman@hotmail.com>
2016-02-15 14:13:16 +0100

    Added solution for Zad. 2

---
+++ lab01/zad2/src/com/example/junit/Calculator.java
@@ -0,0 +1,40 @@
+package com.example.junit;
+
+public class Calculator {
+
+	public double add(double n1, double n2){
+		
+		return n1 + n2;
+		
+	}
+	
+	public double sub(double n1, double n2){
+
+		return n1 - n2;
+		
+	}
+	
+	public double multi(double n1, double n2){
+		
+		return n1 * n2;
+		
+	}
+	
+	public double div(double n1, double n2) {
+		
+		return n1 / n2;
+		
+	}
+	
+	public boolean greater(double n1, double n2){
+		
+		if (n1 > n2){
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+	
+}
