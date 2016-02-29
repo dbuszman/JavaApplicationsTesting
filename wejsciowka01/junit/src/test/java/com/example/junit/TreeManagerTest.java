2f0a8213c69dd329ce0f9047e4ecf2ad96bc1caa
Dariusz Buszman <dbuszman@hotmail.com>
2016-02-29 13:19:45 +0100

    add working test

--- wejsciowka01/junit/src/test/java/com/example/junit/TreeManagerTest.java
+++ wejsciowka01/junit/src/test/java/com/example/junit/TreeManagerTest.java
@@ -8,15 +8,22 @@ public class TreeManagerTest {
 	
 
 	TreeManager treeManager = new TreeManager();
+	
+	private final static String NAME_1 = "Jablon";
+	private final static String TYPE_1 = "lisciaste";
+	private final static int AMOUNT_1 = 5;
 
 	@Test
 	public void checkAdding(){
 
-		Tree drzewo = new Tree("Jablon", "lisciaste", 2);
+		Tree drzewo = new Tree(NAME_1, TYPE_1, AMOUNT_1);
 		
 		treeManager.addTree(drzewo);
 		
-		assertEquals(NAME_2, positionRetrieved.getName());
+		assertEquals(NAME_1, treeManager.trees.get(0).getName());
+		assertEquals(TYPE_1, treeManager.trees.get(0).getType());
+		assertEquals(AMOUNT_1, treeManager.trees.get(0).getAmount());
+		
 	}
 	
 }
