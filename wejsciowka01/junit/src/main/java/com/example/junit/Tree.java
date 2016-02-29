07b4cb43d9f55e9c2fd98e2d0c35f9fdb2730f8b
Dariusz Buszman <dbuszman@hotmail.com>
2016-02-29 12:48:06 +0100

    add Tree class

--- wejsciowka01/junit/src/main/java/com/example/junit/Tree.java
+++ wejsciowka01/junit/src/main/java/com/example/junit/Tree.java
@@ -1,5 +1,46 @@
 package com.example.junit;
 
 public class Tree {
+	
+	private String name;
+	
+	private String type;
+	
+	private int amount;
+	
+	public Tree(){
+	}
+	
+	public Tree(String name, String type, int amount){
+		this.name = name;
+		this.type = type;
+		this.amount = amount;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+
 
 }
