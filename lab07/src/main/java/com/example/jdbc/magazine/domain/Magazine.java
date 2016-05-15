5c72c79e925e721a7b4dbe2029d76f00d183cd6d
Dariusz Buszman <dbuszman@hotmail.com>
2016-05-15 22:27:38 +0200

    Cleaned repo

---
+++ lab07/src/main/java/com/example/jdbc/magazine/domain/Magazine.java
@@ -0,0 +1,54 @@
+package com.example.jdbc.magazine.domain;
+
+public class Magazine {
+
+	private long idPosition;
+
+	private String name;
+
+	private int amount;
+
+	private int margin;
+
+	public Magazine() {
+	}
+
+	public Magazine(String name, int amount, int margin) {
+		this.name = name;
+		this.amount = amount;
+		this.margin = margin;
+	}
+
+	public long getId() {
+		return idPosition;
+	}
+
+	public void setId(long idPosition) {
+		this.idPosition = idPosition;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public int getMargin() {
+		return margin;
+	}
+
+	public void setMargin(int margin) {
+		this.margin = margin;
+	}
+
+}
---
+++ lab07/src/test/java/com/example/jdbc/magazine/manager/ToOrderManagerTest.java
@@ -0,0 +1,129 @@
+package com.example.jdbc.magazine.manager;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.example.jdbc.magazine.domain.Magazine;
+import com.example.jdbc.magazine.domain.ToOrder;
+
+public class ToOrderManagerTest {
+
+	ToOrderManager toOrderManager = new ToOrderManager();
+	MagazineManager magazineManager = new MagazineManager();
+
+	private final static int ORDEREDAMOUNT_2 = 5;
+	private final static float PRICE_2 = 500;
+
+	private final static int ORDEREDAMOUNT_1 = 5;
+	private final static float PRICE_1 = 500;
+
+	private final static String NAME_1 = "Intel SSD";
+	private final static int AMOUNT_1 = 5;
+	private final static int MARGIN_1 = 10;
+
+	Magazine position = new Magazine(NAME_1, AMOUNT_1, MARGIN_1);
+
+	@Before
+	public void prepareForeignKey() {
+		magazineManager.addPosition(position);
+	}
+
+	@After
+	public void cleanUp() throws SQLException {
+		toOrderManager.removeOrders();
+		magazineManager.removePositions();
+	}
+
+	@Test
+	public void checkConnection() {
+		assertNotNull(toOrderManager.getConnection());
+	}
+
+	@Test
+	public void checkUpdatingFK() throws SQLException {
+
+		ToOrder order = new ToOrder(ORDEREDAMOUNT_1, PRICE_1);
+
+		assertEquals(1, toOrderManager.addOrder(order));
+
+		toOrderManager.updateForeignKeyToNull(order);
+
+		List<ToOrder> orders = toOrderManager.getAllOrders();
+		ToOrder orderRetrieved = orders.get(0);
+
+		assertNotNull(orderRetrieved.getMagazineId());
+	}
+
+	@Test
+	public void checkUpdatingFK2() throws SQLException {
+
+		ToOrder order = new ToOrder(ORDEREDAMOUNT_2, PRICE_2);
+
+		assertEquals(1, toOrderManager.addOrder(order));
+
+		toOrderManager.updateForeignKey(order, position);
+
+		assertEquals(order.getMagazineId(), position.getId());
+	}
+
+	@Test
+	public void checkAdding() throws SQLException {
+
+		ToOrder order = new ToOrder(ORDEREDAMOUNT_1, PRICE_1);
+
+		assertEquals(1, toOrderManager.addOrder(order));
+
+		List<ToOrder> orders = toOrderManager.getAllOrders();
+		ToOrder orderRetrieved = orders.get(0);
+
+		assertEquals(ORDEREDAMOUNT_1, orderRetrieved.getOrderedAmount());
+		assertEquals(PRICE_1, orderRetrieved.getPrice(), 0.0d);
+	}
+
+	@Test
+	public void checkRemovingOneElement() throws SQLException {
+
+		ToOrder order = new ToOrder(ORDEREDAMOUNT_1, PRICE_1);
+
+		assertEquals(1, toOrderManager.addOrder(order));
+		assertEquals(0, toOrderManager.removeOneOrder(order));
+	}
+
+	@Test
+	public void checkCountingElements() throws SQLException {
+
+		ToOrder order = new ToOrder(ORDEREDAMOUNT_1, PRICE_1);
+		ToOrder order2 = new ToOrder(ORDEREDAMOUNT_2, PRICE_2);
+
+		toOrderManager.addOrder(order);
+		toOrderManager.addOrder(order2);
+
+		int countingResult = toOrderManager.getCount();
+
+		assertEquals(2, countingResult);
+	}
+
+	@Test
+	public void checkRemovingAllElements() throws SQLException {
+
+		ToOrder order = new ToOrder(ORDEREDAMOUNT_1, PRICE_1);
+		ToOrder order2 = new ToOrder(ORDEREDAMOUNT_2, PRICE_2);
+
+		toOrderManager.addOrder(order);
+		toOrderManager.addOrder(order2);
+
+		toOrderManager.removeOrders();
+
+		int countingResult = toOrderManager.getCount();
+
+		assertEquals(0, countingResult);
+	}
+
+}
