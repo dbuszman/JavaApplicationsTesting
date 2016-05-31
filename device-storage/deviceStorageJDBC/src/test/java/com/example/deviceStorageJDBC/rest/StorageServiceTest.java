2387bd030a95debb702ab75dfad4982b18377544
Dariusz Buszman <dbuszman@hotmail.com>
2016-06-01 00:08:20 +0200

    refactor test class and finished checkUpdatePosition

--- device-storage/deviceStorageJDBC/src/test/java/com/example/deviceStorageJDBC/rest/StorageServiceTest.java
+++ device-storage/deviceStorageJDBC/src/test/java/com/example/deviceStorageJDBC/rest/StorageServiceTest.java
@@ -5,6 +5,7 @@ import static com.jayway.restassured.RestAssured.expect;
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,11 +15,13 @@ import static com.jayway.restassured.RestAssured.*;
 import javax.ws.rs.core.MediaType;
 
 import org.codehaus.jettison.json.JSONException;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
 import com.example.deviceStorageJDBC.domain.Storage;
 import com.jayway.restassured.RestAssured;
@@ -29,7 +32,12 @@ public class StorageServiceTest {
 	private final static int AMOUNT_1 = 5;
 	private final static int MARGIN_1 = 10;
 	
-	private List<Storage> allDevicesFromRest;
+	private final static String NAME_2 = "Intel SSD 750";
+	private final static int AMOUNT_2 = 1;
+	private final static int MARGIN_2 = 8;
+	
+	private final static int DEFAULT_MARGIN = 15;
+	private final static int MIN_AMOUNT = 2;
 	
 	@BeforeClass
 	public static void setUp(){
@@ -38,28 +46,44 @@ public class StorageServiceTest {
 		RestAssured.basePath = "/deviceStorageJDBC/api";
 	}
 	
-	private List<Storage> getAllDevices(){
+	private Storage prepareDevice(JsonValue item){
 		
 		long idPosition;
 		String name;
 		int amount;
 		int margin;
 		
+		idPosition = Long.parseLong(item.asObject().getString("idPosition", "Unknown Item"));
+		name = item.asObject().getString("name", "Unknown Item");
+		amount = Integer.parseInt(item.asObject().getString("amount", "Unknown Item"));
+		margin = Integer.parseInt(item.asObject().getString("margin", "Unknown Item"));
+		
+		return new Storage(idPosition, name, amount, margin);
+	}
+	
+	private List<Storage> getAllDevices(){
+		
 		List<Storage> allDevices = new ArrayList<Storage>();
 		
 		String json = get("/storage/all").asString();
 		
-		JsonArray items = Json.parse(json).asObject().get("storage").asArray();
 		
-		for (JsonValue item : items) {
+		JsonValue jsonValue = Json.parse(json);
+		
+		if(jsonValue.isArray()){
+			JsonArray items = jsonValue.asObject().get("storage").asArray();
 			
-			idPosition = Long.parseLong(item.asObject().getString("idPosition", "Unknown Item"));
-			name = item.asObject().getString("name", "Unknown Item");
-			amount = Integer.parseInt(item.asObject().getString("amount", "Unknown Item"));
-			margin = Integer.parseInt(item.asObject().getString("margin", "Unknown Item"));
-			
-			allDevices.add(new Storage(idPosition, name, amount, margin));
+			for (JsonValue item : items) {
+				
+				allDevices.add(prepareDevice(item));
+			}
 		}
+		else{
+			JsonObject item = jsonValue.asObject().get("storage").asObject();
+			
+			allDevices.add(prepareDevice(item));
+		}
+		
 		return allDevices;
 	}
 	
@@ -78,7 +102,8 @@ public class StorageServiceTest {
 	    then().
 	    	assertThat().statusCode(201);
 		
-		expect().body("storage.size()", equalTo(4)).when().get("/storage/all");
+		List<Storage> allDevicesFromRestAfterAdd = getAllDevices();
+		assertEquals(allDevicesFromRestAfterAdd.size(), 1);
 	}
 	
 	@Test
@@ -92,14 +117,23 @@ public class StorageServiceTest {
 	@Test
 	public void checkUpdatingPositionInStorage() throws JSONException{
 		
-		Storage storage = new Storage(NAME_1, AMOUNT_1, MARGIN_1);
-		Storage storage2 = new Storage(NAME_1, AMOUNT_1, MARGIN_1);
+		delete("/storage").then().assertThat().statusCode(200);
+		
+		Storage storage = new Storage(NAME_2, AMOUNT_2, MARGIN_2);
 		given().contentType(MediaType.APPLICATION_JSON).body(storage).when().post("/storage/");
-		given().contentType(MediaType.APPLICATION_JSON).body(storage2).when().post("/storage/");
 		
-		allDevicesFromRest = getAllDevices();
+		given().
+	       contentType(MediaType.APPLICATION_JSON).
+	       body(storage).
+	    when().	     
+	    	put("/storage/" + DEFAULT_MARGIN + "/" + MIN_AMOUNT).
+	    then().
+	    	assertThat().statusCode(200);
 		
+		List<Storage> allDevicesFromRestAfterUpdate = getAllDevices();
 		
+		int marginAfterUpdate = allDevicesFromRestAfterUpdate.get(0).getMargin();
 		
+		assertEquals(DEFAULT_MARGIN, marginAfterUpdate);
 	}
 }
