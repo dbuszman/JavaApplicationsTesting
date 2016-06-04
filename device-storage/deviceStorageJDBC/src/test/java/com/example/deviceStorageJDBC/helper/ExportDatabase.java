a09a49fcc4703184e2dae94b3e764516c140832b
Dariusz Buszman <dbuszman@hotmail.com>
2016-06-04 23:14:41 +0200

    little correct

--- device-storage/deviceStorageJDBC/src/test/java/com/example/deviceStorageJDBC/helper/ExportDatabase.java
+++ device-storage/deviceStorageJDBC/src/test/java/com/example/deviceStorageJDBC/helper/ExportDatabase.java
@@ -4,12 +4,10 @@ import java.io.FileOutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.xml.FlatDtdDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
-import org.dbunit.ext.hsqldb.HsqldbDataTypeFactory;
 
 public class ExportDatabase {
 
@@ -21,9 +19,6 @@ public class ExportDatabase {
 				"jdbc:hsqldb:hsql://localhost/workdb", "sa", "");
 		
 		connection = new DatabaseConnection(jdbcConnection);
-		
-		DatabaseConfig dbconfig = connection.getConfig();
-		dbconfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new HsqldbDataTypeFactory());
 		
 		FlatDtdDataSet.write(connection.createDataSet(), new FileOutputStream(
 				"src/test/resources/dataSet.dtd"));
